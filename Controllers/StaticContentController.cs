fae3744b2a3c4adc4236c189d45b9aee9ca272c6
Honey <honeyhoper21@gmail.com>
2020-05-08 17:31:29 +0530

    Added StaticContentController

---
+++ Controllers/StaticContentController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace WebAPIApp.Controllers
+{
+    [Authorize]
+    [ApiController]
+    public class StaticContentController : ControllerBase
+    {
+        [HttpGet("doggy")]
+        public IActionResult GetDoggy()
+        {
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "dog.jpg");
+            return PhysicalFile(filePath, "image/jpeg");
+        }
+    }
+}
